3b4c78d38352b959a09e0f1858636a451ea502bb
kapil091 <chauhankapil091@gmail.com>
2023-02-24 03:21:50 +0530

    added header and footer

---
+++ src/components/Header.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import '../Css/_header.css'
+
+export const Header = () => {
+    return (
+        <div className="header-card">
+            <span>
+                header
+            </span>
+        </div>
+    );
+}
